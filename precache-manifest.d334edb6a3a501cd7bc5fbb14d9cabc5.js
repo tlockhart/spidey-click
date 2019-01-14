b8554a83b318f48af7c6e9352deb45f9a85e940c
tlockhart <tony.lockhart@ymail.com>
2019-01-14 13:47:45 -0500

    Updates

---
+++ precache-manifest.d334edb6a3a501cd7bc5fbb14d9cabc5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bffb4a5e21eff023457e58d7c79975c",
+    "url": "/spidey-click/static/media/TheAmazingSpider-Man.6bffb4a5.ttf"
+  },
+  {
+    "revision": "fa581c0dc718e2fa46eec95ed5286b03",
+    "url": "/spidey-click/static/media/SF Comic Script Condensed.fa581c0d.ttf"
+  },
+  {
+    "revision": "f85072630ace90862c4690f1d61ece93",
+    "url": "/spidey-click/static/media/KGHAPPYSolid.f8507263.ttf"
+  },
+  {
+    "revision": "c92fe3054b69933cd7c9",
+    "url": "/spidey-click/static/js/runtime~main.c92fe305.js"
+  },
+  {
+    "revision": "4738fa630a1b6e01774b",
+    "url": "/spidey-click/static/js/main.4738fa63.chunk.js"
+  },
+  {
+    "revision": "4a4cf2359ecd8a3b1537",
+    "url": "/spidey-click/static/js/1.4a4cf235.chunk.js"
+  },
+  {
+    "revision": "4738fa630a1b6e01774b",
+    "url": "/spidey-click/static/css/main.0ec2782b.chunk.css"
+  },
+  {
+    "revision": "7a4fcb9916ed74307c5f3850207a6647",
+    "url": "/spidey-click/index.html"
+  }
+];
