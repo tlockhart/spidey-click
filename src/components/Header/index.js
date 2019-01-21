ef8e9ad95a44946b1cf4398038575b739ed2489e
tlockhart <tony.lockhart@ymail.com>
2019-01-20 19:07:52 -0500

    fixed problem with icon on deployed version

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header(props){
         <React.Fragment>
             <nav className="navbar navbar-expand-md navbar-dark fixed-top text-center">
                 <ul className="navbar-nav mx-auto">
-                    <li className="brand nav-item"><a href="/"><h3 className="icon">@</h3></a></li>
+                    <li className="brand nav-item"><a href="/spidey-click/"><h3 className="icon">@</h3></a></li>
                     <li className="nav-item"><p className="instructions" id="msg">Click an image to begin!</p></li>
                     <li className="nav-item"><h3 className="score">Score: {props.score} | Top Score: {props.topScore}</h3> </li>
                 </ul>
